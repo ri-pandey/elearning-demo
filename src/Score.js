226ede63c3cc4899eb24f0ffc728a4b4a8c06b26
ripandey <ripandey299@gmail.com>
2023-02-28 19:12:51 -0500

    changed wording

--- src/Score.js
+++ src/Score.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import {Alert} from "rivet-react";
+import {Alert, Panel} from "rivet-react";
 
 export const Score = ({questions}) => {
   const maxScore = questions.length
   const calculatedScore = questions.filter(e => e.isAnsweredCorrectly).length
 
-  return <Alert variant={"info"} title={"Finished"}>You scored {calculatedScore}/{maxScore}</Alert>
+  return <Panel><Alert variant={"info"} title={"Assessment Complete"}>You scored {calculatedScore}/{maxScore} in this assessment</Alert></Panel>
 }
