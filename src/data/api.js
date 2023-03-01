65d4f1749a4e77bc559cd3223450a427aed02ba1
ripandey <ripandey299@gmail.com>
2023-02-28 19:08:56 -0500

    refactor

--- src/data/api.js
+++ src/data/api.js
@@ -1,14 +1,7 @@
 import {questionsWithCorrectOptions} from "./data";
+import {arraysAreEqual} from "../util";
 
 export const submitResponse = (question) => {
-  const arraysAreEqual = (arr1, arr2) => {
-    if ((arr1 === null || arr2 === null) ||
-      (arr1.length !== arr2.length)) {
-      return false
-    }
-    return arr1.every((e, i) => e === arr2[i])
-  }
-
   return new Promise((resolve, reject) => {
     let selectedOptions = question.options.filter(e => !!e.selected).map(e => e.id)
     let correctOptions = questionsWithCorrectOptions.find(e => e.id === question.id).correctOptions
--- src/util.js
+++ src/util.js
@@ -3,3 +3,11 @@ export const STATUS = {
   IN_PROGRESS: 1,
   FINISHED: 2
 }
+
+export const arraysAreEqual = (arr1, arr2) => {
+  if ((arr1 === null || arr2 === null) ||
+    (arr1.length !== arr2.length)) {
+    return false
+  }
+  return arr1.every((e, i) => e === arr2[i])
+}
